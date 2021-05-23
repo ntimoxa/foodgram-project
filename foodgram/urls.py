b6a7c542655b4e94ca67c5b456e0791660478af3
Timofey <tim-pro7@mail.ru>
2021-05-23 18:23:20 +0500

    Flake8

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import handler404, handler500
 
 handler404 = 'recipes.views.page_not_found'
 handler500 = 'recipes.views.server_error'
