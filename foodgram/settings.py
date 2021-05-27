27ff2a57edeccf59e4524eb15c44df51a1a5ffe9
ntimoxa <70896848+ntimoxa@users.noreply.github.com>
2021-05-28 01:16:48 +0500

    Update settings.py

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-58j6_-1h151)z9&b5zpzm*dsp9w*9s9l&&^h%jj-+72%)&7o1&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
