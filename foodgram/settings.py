7e4040a6620363e196bf4a7906f9bbc289d86314
Тимофей <tim-pro7@mail.ru>
2021-07-02 00:06:14 +0500

    Checking workflow

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': '468469q',
         'HOST': 'db',
         'PORT': 5432,
     }
